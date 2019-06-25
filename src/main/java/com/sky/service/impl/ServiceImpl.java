a76ae590bfc7648b05bdc9e9833c3aae85131068
jiafei <jiafei@migu.cn>
2019-06-25 15:21:34 +0800

    commit1

--- src/main/java/com/sky/service/impl/ServiceImpl.java
+++ src/main/java/com/sky/service/impl/ServiceImpl.java
@@ -1,4 +1,10 @@
 package com.sky.service.impl;
 
-public class ServiceImpl {
+import com.sky.service.IService;
+
+public class ServiceImpl implements IService {
+    @Override
+    public void Fun1(String i) {
+        
+    }
 }
